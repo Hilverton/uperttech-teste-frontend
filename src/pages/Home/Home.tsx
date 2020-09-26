49f2c694f6a892185d4e2e5f314e9a79ce7c2d8c
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-26 16:00:55 -0300

    Correção do alinhamento do código da página Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,15 +9,13 @@ export default function Home() {
   return (
     <Container>
       <Main>
-
-          <Content>
-            <Title>Soluções inteligentes para modernizar sua empresa.</Title>
-            <Info>Expertise em gerência de projetos e em desenvolvimento de software para produzir soluções que vão direto ao ponto.</Info>
-            <Link to='/conta'>
-              <Button>Solicite um orçamento</Button>
-            </Link>
-          </Content>
-
+        <Content>
+          <Title>Soluções inteligentes para modernizar sua empresa.</Title>
+          <Info>Expertise em gerência de projetos e em desenvolvimento de software para produzir soluções que vão direto ao ponto.</Info>
+          <Link to='/conta'>
+            <Button>Solicite um orçamento</Button>
+          </Link>
+        </Content>
       </Main>
     </Container>
   );
