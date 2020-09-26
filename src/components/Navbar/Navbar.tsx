0186415de4bdf27b6b093f8027873a1a9d920be5
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-26 14:14:40 -0300

    Componente Navbar responsivo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Container, Logo, Menu, Link } from './styles';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Container, Logo, Menu, Link, Mobile } from './styles';
 
 import Brand from '../../assets/brand.png';
 
 import Button from '../Button';
 
 export default function Navbar() {
+  const location = useLocation();
   const [scrolled, setScrolled] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    setClicked(false);
+  }, [location]);
 
   function changeBackground(): void {
     console.log(window.scrollY);
@@ -16,14 +22,17 @@ export default function Navbar() {
   }
 
   window.addEventListener('scroll', changeBackground)
-    console.log(scrolled);
+  console.log(scrolled);
 
   return (
     <Container className={`${scrolled && 'is-active'}`}>
       <NavLink to='/'>
         <Logo src={Brand} />
       </NavLink>
-      <Menu>
+      <Mobile onClick={() => setClicked(!clicked)}>
+        {!clicked ? <i className="fas fa-bars"></i> : <i className="fas fa-times"></i>}
+      </Mobile>
+      <Menu open={!clicked}>
         <Link to='/' exact activeClassName='is-active'>Quem somos</Link>
         <Link to='/serviços' activeClassName='is-active'>Serviços</Link>
         <NavLink to='/produtos'>
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+import Button from '../Button';
+
+type Props = {
+  open: boolean;
+} 
+
 export const Container = styled.nav`
   width: 100%;
   height: 80px;
@@ -17,17 +23,48 @@ export const Container = styled.nav`
     -moz-box-shadow: var(--navbar-shadow);
     box-shadow: var(--navbar-shadow);
   }
+
+  @media(max-width: 760px) {
+    padding: 25px 0;
+    display: flex;
+    height: auto;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
 export const Logo = styled.img`
   width: 224px;
+
+  @media(max-width: 760px) {
+    width: 170px;
+    margin-left: 20px;
+  }
 `;
 
-export const Menu = styled.ul`
+export const Menu = styled.ul<Props>`
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media(max-width: 760px) {
+    flex-direction: column;
+    width: 100%;
+    position: absolute;
+    justify-content: space-around;
+    height: 200px;
+    align-items: center;
+    margin: 165px 0 0 0;
+    top: -150%;
+    transform: ${({ open }) => open ? 'translateY(-150%)' : 'translateY(0)'};
+    transition: transform 0.5s ease;
+    background-color: var(--white);
+    -webkit-box-shadow: var(--navbar-shadow);
+    -moz-box-shadow: var(--navbar-shadow);
+    box-shadow: var(--navbar-shadow);
+  }
 `;
 
 export const Link = styled(NavLink)`
@@ -39,5 +76,22 @@ export const Link = styled(NavLink)`
   &.is-active {
     font-weight: bold;
   }
+
+  @media(max-width: 760px) {
+    margin-right: 0;
+  }
 `;
+
+export const Mobile = styled(Button)`
+  display: none;
+
+  @media(max-width: 760px) {
+    display: block;
+    position: absolute;
+    top: 1;
+    right: 0;
+    margin-right: 20px;
+  }
+`;
+
 
