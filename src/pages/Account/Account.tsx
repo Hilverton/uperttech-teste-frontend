e7018c48b3ddbc73869a7cce18444af198ed11be
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-28 15:26:08 -0300

    Atualização da página Account, que sofre influência do localStorage

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Forms } from '../../components';
+import { LocalStorage } from '../../utils';
 
 import { Container, Main } from './styles';
 
 export default function Account() {
+  const history = useHistory();
+  useEffect(() => {
+    function check() {
+      const response = LocalStorage.logged();
+      if (response.logged) history.replace('/produtos');
+    }
+
+    check();
+  }, [history]);
+
   return (
     <Container>
       <Main>
